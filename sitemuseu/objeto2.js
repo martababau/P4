4c28dcb83301d98fb98d58fe4de99d5c2e043118
marta <marta.babau@gmail.com>
2024-05-15 18:48:08 +0100

    qqq

---
+++ sitemuseu/objeto2.js
@@ -0,0 +1,18 @@
+function addFloatingImages() {
+  for (var i = 1; i <= 9; i++) {
+    var img = document.createElement("img");
+    img.src = "imagens/chave" + i + ".png";
+    img.classList.add("floating-image");
+    img.style.top = Math.floor(Math.random() * 70) + 10 + "%";
+    img.style.left = Math.floor(Math.random() * 15) + "%";
+    img.style.transform = "rotate(" + Math.floor(Math.random() * 20) + "deg)";
+    document.body.appendChild(img);
+  }
+}
+window.onload = function () {
+  addFloatingImages();
+};
+
+function showVideo(src) {
+  alert("Show the " + src + " video as a pop-up");
+}
