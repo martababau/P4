fb3c88e50a8f9c59d3c0c48ba317d00e20a8cc4e
marta <marta.babau@gmail.com>
2024-05-15 18:28:15 +0100

    aa

---
+++ sitemuseu/objeto3.js
@@ -0,0 +1,21 @@
+var counter = 0;
+var images = [
+  "imagens/per0.png",
+  "imagens/per1.png",
+  "imagens/per2.png",
+  "imagens/per3.png",
+  "imagens/per4.png",
+  "imagens/per5.png",
+  "imagens/per6.png",
+  "imagens/per7.png",
+];
+
+setInterval(function () {
+  document.getElementById("imagem").src =
+    "imagens/per" + (counter % images.length) + ".png";
+  counter += 1;
+}, 200);
+
+function showVideo(src) {
+  alert("Show the " + src + " video as a pop-up");
+}
