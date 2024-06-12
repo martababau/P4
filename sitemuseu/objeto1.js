1c81489365ee8171d72363eacb13d203cb455a8c
Guil913 <guilhermecoelho913@gmail.com>
2024-06-12 19:14:51 +0100

    tudp feotp agpra

--- sitemuseu/objeto1.js
+++ sitemuseu/objeto1.js
@@ -15,16 +15,26 @@ setInterval(function () {
   counter += 1;
 }, 200);
 
-function showVideo(src) {
-  alert("Show the " + src + " video as a pop-up");
-}
+
 
 
 function showPopup() {
   document.getElementById("popup-container").style.display = "block";
 }
 
-
 function closePopup() {
   document.getElementById("popup-container").style.display = "none";
+}
+
+function showVideo(src) {
+  var video = document.getElementById("popup-video");
+  video.src = src;
+  document.getElementById("video-popup-container").style.display = "block";
+}
+
+function closeVideoPopup() {
+  var video = document.getElementById("popup-video");
+  video.pause();
+  video.src = "";
+  document.getElementById("video-popup-container").style.display = "none";
 }
